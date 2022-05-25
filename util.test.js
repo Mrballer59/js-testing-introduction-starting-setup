77a84ab011d294825e3a1f19c980ec331db87ade
B <77552838+Mrballer59@users.noreply.github.com>
2022-05-25 14:46:51 +0200

    first commit

---
+++ util.test.js
@@ -0,0 +1,17 @@
+const { generateText, checkAndGenerate } = require("./util");
+
+test("should output name and age", () => {
+  const text = generateText("Max", 29);
+  expect(text).toBe("Max (29 years old)");
+  //   const text2 = generateText("Jacob", 10);
+  //   expect(text2).toBe("Jacob (10 years old)");
+});
+
+// test("should output data-less test", () => {
+//   const text = generateText("", null);
+//   expect(text).toBe(" (null years old)");
+// });
+test("should generate a valid text output", () => {
+  const text = checkAndGenerate("Max", 29);
+  expect(text).toBe("Max (29 years old)");
+});
